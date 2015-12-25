28659831dba9d5c2d75b65dae75fd80572959fd6
dantetam <dante.tam1@gmail.com>
2015-12-25 10:36:35 -0800

    More LWJGL 3 code

--- src/render/DisplayManager.java
+++ src/render/DisplayManager.java
@@ -3,14 +3,20 @@ package render;
 import static org.lwjgl.glfw.GLFW.*;
 import org.lwjgl.glfw.Callbacks;
 import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWFramebufferSizeCallback;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GLContext;
+import org.lwjgl.util.Display;
+
+import tests.MainGameLoop;
 
 public class DisplayManager {
 
 	private static final int width = 1500, height = 900;
-	public static GLFWErrorCallback errorCallback; 
-	public static long window; 
+	private static GLFWErrorCallback errorCallback; 
+	private static GLFWFramebufferSizeCallback framebufferSizeCallback; 
+	public static long window;
+	public boolean fullscreen = false;
 
 	public static void createDisplay()
 	{
@@ -27,6 +33,18 @@ public class DisplayManager {
 		if (window == 0)
 			throw new RuntimeException("Failed to create window");
 
+		glfwSetFramebufferSizeCallback(window, (framebufferSizeCallback = new GLFWFramebufferSizeCallback() {
+		    @Override
+		    public void invoke(long window, int width, int height) {
+		        onResize(width, height);
+		    }
+		}));
+		onResize(width, height);
+		
+		//GL11.glViewport(0, 0, width, height);
+	}
+	
+	public static void onResize(int width, int height) {
 		GL11.glViewport(0, 0, width, height);
 	}
 
