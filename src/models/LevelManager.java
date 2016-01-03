ee4f5dae6f3bb859b02cb5cb12579b94eb2a28d1
dantetam <dante.tam1@gmail.com>
2016-01-02 22:14:44 -0800

    Synchronize entities and rendered models

--- src/models/LevelManager.java
+++ src/models/LevelManager.java
@@ -2,23 +2,45 @@ package models;
 
 import java.util.ArrayList;
 
+import entity.BaseEntity;
+
 public class LevelManager {
 
-	public ArrayList<TexturedModel> models = new ArrayList<TexturedModel>();
+	public ArrayList<EntityModelLink> links = new ArrayList<EntityModelLink>();
 	
 	public void addEntity()
 	{
 		
 	}
 	
-	public void moveEntity()
+	public void moveEntity(BaseEntity en, int r, int c)
 	{
 		
 	}
 	
-	public void removeEntity()
+	public EntityModelLink removeEntity(BaseEntity en)
 	{
+		for (EntityModelLink link: links)
+		{
+			if (link.entity.equals(en))
+			{
+				links.remove(link); 
+				return link;
+			}
+		}
+		return null; //Could not find element. Handle accordingly.
+	}
+	
+	public class EntityModelLink
+	{
+		public BaseEntity entity;
+		public ArrayList<TexturedModel> models;
 		
+		public EntityModelLink(BaseEntity en)
+		{
+			entity = en;
+			models = new ArrayList<TexturedModel>();
+		}
 	}
 	
 }
